0c8b1f9f0e0808616fa447d363501f7217692135
David Bender <bender2242@gmail.com>
2023-03-12 00:21:30 +0000

    added hover effects to buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,18 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="absolute flex left-0 top-0 pt-4 justify-items-center">
-                    <div className="rounded-md bg-white m-auto p-2 ml-2 cursor-default text-lg">
+                    <div
+                        className="rounded-md bg-white m-auto p-2 ml-2 cursor-default text-lg 
+                                    hover:bg-gray-400 hover:text-white hover:cursor-pointer"
+                    >
                         menu
                     </div>
                 </div>
                 <div className="absolute flex right-0 top-0 pt-4 justify-items-center">
-                    <div className="rounded-md bg-white m-auto p-2 mr-2 cursor-default text-lg">
+                    <div
+                        className="rounded-md bg-white m-auto p-2 mr-2 cursor-default text-lg 
+                                    hover:bg-gray-400 hover:text-white hover:cursor-pointer"
+                    >
                         profile
                     </div>
                 </div>
