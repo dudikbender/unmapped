b03aef4656c36ec6171a06e00a855cc826e35690
David Bender <bender2242@gmail.com>
2023-03-26 04:11:23 +0100

    removed test env variable

--- env.d.ts
+++ env.d.ts
@@ -4,7 +4,6 @@ declare global {
             NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
             CLERK_SECRET_KEY: string;
             NEXT_PUBLIC_MAPBOX_TOKEN: string;
-            TEST: string;
             NEXT_PUBLIC_SUPABASE_URL: string;
             NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
         }
